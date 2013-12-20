ed39f1fa513c766d7ac3419659eb44ae1ae179fe
Jesse Bryan <winneonsword@gmail.com>
2013-12-20 12:12:45 -0600

    jQuery, JavaScript, and completion of main HTML.

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function onLoad(){
+	
+	var id = document.getElementById("title");
+	var text = "to its fullest";
+	
+	innerHTML = text;
+	
+}
+
+function onHover(){
+	
+	var id = document.getElementById("title");
+	var text = "scroll down";
+	
+	id.innerHTML = text;
+		
+}
