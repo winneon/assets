1924e6ab459d838df0fb3655b038bf1cd7d682fa
Jesse Bryan <winneonsword@gmail.com>
2013-12-20 13:02:00 -0600

    Added expirimental mobile browser detection

---
+++ js/detectmobile.js
@@ -0,0 +1,9 @@
+<!--
+	var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+	
+	if (mobile){
+	
+		window.location.replace("http://blog.winneonsword.net");
+		
+	}
+//-->
