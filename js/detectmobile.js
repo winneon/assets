48574b44613d386881016c3c13a6879a96e25391
Jesse Bryan <winneonsword@gmail.com>
2013-12-20 13:14:34 -0600

    A little more testing with mobile

--- js/detectmobile.js
+++
@@ -1,9 +0,0 @@
-<!--
-	var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
-	
-	if (mobile){
-	
-		window.location.replace("http://blog.winneonsword.net");
-		
-	}
-//-->
