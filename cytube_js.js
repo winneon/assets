a58e0498caded35a4d634b7b7d15217bb227b1d5
Jesse Bryan <winneonsword@gmail.com>
2014-04-12 21:54:43 +0200

    Automtic commit.

--- cytube_js.js
+++ cytube_js.js
@@ -15,3 +15,5 @@ var navbar = $(".navbar-nav");
 navbar.html("");
 navbar.append("<li><a href='ts3server://t4i.teamspeak.cc/?port=9071'>Connect to TS</a></li>");
 navbar.append("<li><a id='showchannelsettings' href='#' onclick=\"javascript:$('#channeloptions').modal()\" style>Settings</a></li>");
+
+setVisible("#showchansettings", CLIENT.rank >= 2);
