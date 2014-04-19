b58b5b7759dff97f555d61ed5aaaf8616e6d7de8
Jesse Bryan <winneonsword@gmail.com>
2014-04-19 18:23:03 +0200

    Automtic commit.

--- cytube_js.js
+++ cytube_js.js
@@ -2,9 +2,9 @@
 	$.fn.changeElementType = function(newType){
 		var attrs = { };
 
-		$.each($(this[0].attributes, function(idx, attr)){
+		$.each($(this[0].attributes, function(idx, attr){
 			attrs[attr.nodeName] = attr.nodeValue;
-		});
+		}));
 
 		this.replaceWith(function(){
 			return $("<" + newType + "/>", attrs).append($(this).contents());
