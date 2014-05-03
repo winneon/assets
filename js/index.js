cc8f565da6446c92696009d93061045c2f4014ae
Jesse Bryan <winneonsword@gmail.com>
2014-05-02 22:22:04 -0500

    Renovations

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-function onLoad(){
-	
-	var id = document.getElementById("title");
-	var text = "to its fullest";
-	
-	innerHTML = text;
-	
-}
+var title = $("div#title");
 
-function onHover(){
-	
-	var id = document.getElementById("title");
-	var text = "scroll down";
-	
-	id.innerHTML = text;
-		
-}
+title.html(title.attr("out"));
+title.hover(function(){
+	title.html(title.attr("in"));
+}, function(){
+	title.html(title.attr("out"));
+});
+
+$.fn.animateScroll = function(id){
+	$(this).click(function(){
+		$("html, body").animate({
+			scrollTop: $(id).offset().top
+		}, 2000);
+	});
+};
+
+$(".home").animateScroll("#top");
+$(".projects").animateScroll("#projects");
