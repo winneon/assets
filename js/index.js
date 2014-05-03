68e41c294d6d9cc88a8ef72eb510a04daadf0b1d
Jesse Bryan <winneonsword@gmail.com>
2014-05-03 01:46:25 -0500

    Added a top button.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
-$.fn.animateScroll = function(id){
-	$(this).click(function(){
-		$("html, body").animate({
-			scrollTop: $(id).offset().top
-		}, 2000);
+$(function(){
+	$("a[href*=#]:not([href=#])").click(function(){
+		if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname){
+			var target = $(this.hash);
+			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+			if (target.length){
+				$("html, body").animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
 	});
-};
+	$("a[href=#top]").click(function(){
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
+		return false;
+	});
+});
 
 var title = $("div#title");
 
