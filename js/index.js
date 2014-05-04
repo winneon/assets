285681fc3e1e9587ec9408252843f6667b1c405d
Jesse Bryan <winneonsword@gmail.com>
2014-05-04 15:07:16 -0500

    Added some fancy scrollbars

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 $(function(){
+	var page = $("html, body");
+	
 	$("a[href*=#]:not([href=#])").click(function(){
 		if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname){
 			var target = $(this.hash);
 			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
 			if (target.length){
-				$("html, body").animate({
+				page.animate({
 					scrollTop: target.offset().top
 				}, 1000);
 				return false;
@@ -12,18 +14,32 @@ $(function(){
 		}
 	});
 	$("a[href=#top]").click(function(){
-		$('html, body').animate({
+		page.animate({
 			scrollTop: 0
 		}, 1000);
 		return false;
 	});
+	
 });
 
 var title = $("div#title");
-
 title.html(title.attr("out"));
+
 title.hover(function(){
 	title.html(title.attr("in"));
 }, function(){
 	title.html(title.attr("out"));
+});
+
+$(".project-title").after("<br />");
+
+$(document).ready(function(){
+	$("html").niceScroll({
+		styler: "fb",
+		cursorcolor: "#2D2D2D",
+		cursorborder: "0px solid black",
+		scrollspeed: "40",
+		cursoropacitymin: "1",
+		cursoropacitymax: "1"
+	});
 });
