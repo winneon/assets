9b11efd306acab36688f83c5221d8f081ff2c54a
Jesse Bryan <winneonsword@gmail.com>
2014-05-16 11:38:33 -0500

    Cytube Updates.

--- js/cytube_js.js
+++ js/cytube_js.js
@@ -48,4 +48,4 @@ $(".credit").html("Copyright &copy; 2012-2014 <a href='http://ohsototes.com'>Wor
 var width = ($(document).width() - 40) + "";
 var height = Math.floor(parseInt(width) * 9 / 16 + 1) + "";
 
-$("ytapiplayer").width(width).height(height);
+alert($("ytapiplayer").width(width).height(height));
