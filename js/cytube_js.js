8388de6540e92ecfa8a08dd73ad7bb7409034d3c
Jesse Bryan <winneonsword@gmail.com>
2014-05-16 11:45:08 -0500

    Cytube Updates.

--- js/cytube_js.js
+++ js/cytube_js.js
@@ -45,7 +45,7 @@ $("#mediarefresh").prepend("<span id='button-text'>Refresh Player</span>");
 $("#currenttitle").changeElementType("div");
 $(".credit").html("Copyright &copy; 2012-2014 <a href='http://ohsototes.com'>Worlds Collide Network</a> &#8226; Hosted on <a href='http://cytu.be'>CyTu.be</a>");
 
-resizeStuff = function(){
+function resizeStuff(){
 	var width = ($(document).width() - 40) + "";
 	var height = Math.floor(parseInt(width) * 9 / 16 + 1) + "";
 	
