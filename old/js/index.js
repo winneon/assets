354c08f819606f6c9245154dca5d9b500b273015
Jesse Bryan <winneonsword@gmail.com>
2014-06-28 16:35:39 -0500

    New website design!

---
+++ old/js/index.js
@@ -0,0 +1,46 @@
+$(function(){
+	var page = $("html, body");
+	
+	$("a[href*=#]:not([href=#])").click(function(){
+		if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname){
+			var target = $(this.hash);
+			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+			if (target.length){
+				page.animate({
+					scrollTop: target.offset().top - 70
+				}, 1000);
+				return false;
+			}
+		}
+	});
+	$("a[href=#top]").click(function(){
+		page.animate({
+			scrollTop: 0
+		}, 1000);
+		return false;
+	});
+	
+});
+
+var title = $("div#title");
+title.html(title.attr("out"));
+
+title.hover(function(){
+	title.html(title.attr("in"));
+}, function(){
+	title.html(title.attr("out"));
+});
+
+$("a").attr("target", "_blank");
+
+var project_title = $("div.project > a:first-child");
+project_title.after("<br />");
+
+$("html").niceScroll({
+	styler: "fb",
+	cursorcolor: "#2D2D2D",
+	cursorborder: "0px solid black",
+	scrollspeed: "40",
+	cursoropacitymin: "1",
+	cursoropacitymax: "1"
+});
